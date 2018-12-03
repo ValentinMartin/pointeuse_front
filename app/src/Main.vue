22854defc08448d1094729444facf5c1ed6a21f5
valentinMartin <valentin.martinpro@gmail.com>
2018-12-03 10:07:28 +0100

    test

--- app/src/Main.vue
+++ app/src/Main.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test msg="Salut Mokaccino, Bienvenue sur le Front !"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
