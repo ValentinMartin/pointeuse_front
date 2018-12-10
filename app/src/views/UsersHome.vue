5fbd0479f2f90375b9b55e67a39c6d504f0a4f2d
valentinMartin <valentin.martinpro@gmail.com>
2018-12-10 16:52:03 +0100

    Create UsersHome.vue

---
+++ app/src/views/UsersHome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="content">
+    <p>Utilisateurs</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersHome',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
